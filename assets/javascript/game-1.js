5633c8e2748d06e2357b2d369b5cc69edd51885c
Namita Shenai <namita.shenai@gmail.com>
2019-02-27 21:19:24 -0800

    Set the timer for the Trivia Quiz

--- assets/javascript/game-1.js
+++ assets/javascript/game-1.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	var userAnswer; 
 	var timeRemaining = 60; //a minute 
 	var interValid; 
-	var clockRunning = true;
+	var clockRunning = false;
 
 	//Object list 
 	var triviaQuestions =[ {
@@ -40,45 +40,56 @@ $(document).ready(function(){
 
 	//Takes seconds and element as its parameter 
 	function countDown(){
+		 //  TODO: Use setInterval to start the count here and set the clock to running.
+		 if (!clockRunning) {
+			clockRunning = true;
 
-		if (clockRunning) {
-			
+			if( timeRemaining === 0 ){
+				// Countdown stops.
+				stopCountDown();
+				$("#timer").html("<h2> OOPS!! times up!</h2>");
+			}
+
 			timeRemaining--; //Decrement the value 
 
 			//Display time is this format 00:00 
 			var quizTime = timeConverter(timeRemaining); 
 			$("#timer").html("<h2>" + quizTime + "</h2>");
 
-			console.log("timeRemaining: "+ timeRemaining);
+			// console.log("timeRemaining: "+ timeRemaining);
 
 			interValid = setInterval(countDown,1000);//run after every second 
-
-			if( timeRemaining === 0 ){
-				//stopt the clock 
-				stopCountDown(); 
-				// Timer is replaces with text
-				$("#timer").html("<h4>ALL DONE</h4>");
-				// Quiz is replaced with scores/quiz data
-				$("#quiz-container").html("<h4>Correct Answers: " + quiz.correctAnswers + "</h4>");
-				$("#quiz-container").append("<h4>Wrong Answers: " + quiz.wrongAnswers + "</h4>");
-				
-			}
 		}
-	
+		console.log(clockRunning);
 	}
-
-	//Resets the clock to stop 
 
 	function stopCountDown(){
-		// console.log("stopCountDown");
-
 		clearInterval(interValid);
-		clockRunning = false;  //Reset 
-		timeRemaining = 60; //Reset timer 
-
+		clockRunning = false; 
 	}
 	
-	//Returns the time in this formnat 00:00 
+	function renderQuestionAns() {
+
+		console.log('renderQuestionAns' + triviaQuestions.length);
+		// populates the quiz questions.
+		
+		for(var i =0; i < triviaQuestions.length; i++){
+			
+			console.log(triviaQuestions[i].question);
+			$("#questionBlock").html( "<center><p id='questiontext'> "+ triviaQuestions[i].question + " <br/></p></center>"); 
+			console.log(triviaQuestions[i].choices);
+			// $("#answerBlock").html("<p id='anstext'> " + triviaQuestions[i].choices +" </p>"); 
+			console.log(triviaQuestions[i].choices.length);
+			for (j = 0; j < triviaQuestions[i].choices.length; j++) {
+				$('<input id="answers" class="radio" type="radio" name="dynradio' + (i) + '" value="' + (j) + '">' + triviaQuestions[i].choices[j] + '</input><br/>').appendTo("#answerBlock");
+			  }
+		}
+		// else {
+		// 	$("#questionBlock").innerHTML  = "<p> Game over!! </p>"; 
+		// 	// document.getElementById("score").innerHTML = "Final Score: " + score + " out of " + quizList.length
+		// }
+	}
+
 	function timeConverter(t) {
 
 		//  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
@@ -98,14 +109,22 @@ $(document).ready(function(){
 		}
 	
 		return minutes + ":" + seconds;
-	}
+	  }
 
 	// Main PROCESS -  functions
 	//--------------------------------------
 
 	console.log( "ready!" );
 
-	//Calling the Timer function 
-	countDown(); 
+	countDown(); //runit for 60 secs passing the element 
 
+	// Calling functions to start the game.
+	renderQuestionAns();
+
+	// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+	document.onkeyup = function(event) {
+            userAnswer = event.key; 
+	
+	}
+			
 });	
