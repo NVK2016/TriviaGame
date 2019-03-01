b4db8fa133235cbd0e94a8f48ffba9d12dd9c68f
Namita Shenai <namita.shenai@gmail.com>
2019-02-28 20:10:34 -0800

    rearranged the logic and bifurcated the functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
 	var unasweredCount = 0;
 	var currentQuestion = 0;
 	var currentAnswer;
-	var timeRemaining = 30; //a minute 
-	var interValid;
+	var timeRemaining = 15; //a minute 
+	var interValid, showQA;
 	var clockRunning = true;
 	var answeredBtnClicked = false;
 
@@ -21,7 +21,7 @@ $(document).ready(function () {
 			question: "Pupusas, handmade thick stuffed corn tortillas, are a traditional dish from what country?",
 			choices: ["Ethiopia", "El Salvadore", "Peru", "Guatamala"],
 			correctAnswer: 1,
-			imageLink: "assets/images/pupusas.jpg"
+			imageLink: "assets/images/logo_JavaScript.png"
 		},
 		{
 			question: "Who Loves Orange Soda?",
@@ -29,6 +29,12 @@ $(document).ready(function () {
 			correctAnswer: 0,
 			imageLink: "assets/images/loading.gif"
 		},
+		{
+			question: "In the cartoon series, the Flintstones, what was the Great Gazoo?",
+			choices: ["A Dragon", "A club", "An alien", "a giraffee"],
+			correctAnswer : 2,
+			imageLink: "assets/iamges/flintstones.png"
+		}
 	];
 
 	//     FUNCTIONS
@@ -48,21 +54,30 @@ $(document).ready(function () {
 			if (timeRemaining === 0) {
 				//stop the clock 
 				stopCountDown();
-				//INDEX IS 
-				console.log(currentQuestion + " " + triviaQuestions.length );
-				//Move to next Question 
+				// timeRemaining = 15;
+				// Player doesnt answer and time runs out 
+				if(( !answeredBtnClicked) ){
+					timeOuts(); 
+				}
 				
-			}
+				//Move to next Question 
+				nextQuestion(); 
+			} 
+			
 		}
+	}
 
+	function nextQuestion(){
+	
+		currentQuestion++;
+		showQA = setInterval(renderQuestionAns, 1000);
 	}
 
 	//Resets the clock to stop 
 	function stopCountDown() {
-
+		timeRemaining = 0; 
 		clearInterval(interValid);
-		clockRunning = false;  //Reset 
-		timeRemaining = 60; //Reset timer 
+		clockRunning = false;  //Reset  
 
 	}
 
@@ -89,15 +104,18 @@ $(document).ready(function () {
 	}
 
 	function renderQuestionAns() {
-
+	
 		//Continue the timer clock 
 		interValid = setInterval(countDown, 1000);//run after every second 
+		// clearInterval(showQA); 
+
 		//Display Question 
 		$("#questionBlock").text(triviaQuestions[currentQuestion].question);
 		console.log("Question#: " + currentQuestion);
 		//Dynamically creating buttons 
 		dynamicAnswerBtn(); 
-
+		console.log("timeRemaining: "+ timeRemaining + " answeredBtnClicked:"+ answeredBtnClicked);
+		
 	}
 
 	function dynamicAnswerBtn() {
@@ -112,16 +130,16 @@ $(document).ready(function () {
 		}
 		//RIGHT ANSWER FOR THE QUESTION 
 		currentAnswer = triviaQuestions[currentQuestion].correctAnswer;
-		console.log(currentAnswer); 
+		// console.log(currentAnswer ); 
 
 		//Add event listiner 
 		$(".thisChoice").click(function () {
+
 			answeredBtnClicked = true;
-
 			var id = $(this).attr('data-index');
+			// console.log(answeredBtnClicked + " ID: " + $(this).attr('data-index') + "Correct ans:" + currentAnswer);
 
-			console.log(answeredBtnClicked + " ID: " + id);
-			if (id === currentAnswer) {
+			if ( id == currentAnswer) {
 				//In case if the answer is correct 
 				correctAnswers();
 			} else {
@@ -132,37 +150,55 @@ $(document).ready(function () {
 	}
 
 	function correctAnswers() {
+
+		console.log("Correct ans fun"+ timeRemaining);
+		
 		//stop the clock 
 		stopCountDown(); 
 		//Count the correct answer 
         correctCount++;
+
 		//Display message 
+		$("#timer").html("<h2> Time Remaining: 00:00 </h2> <br/>");
         $('#questionBlock').html("Wohoo! Awesome you knew it! <br/>");
         $('#questionBlock').append("THE ANSWER IS: " + triviaQuestions[currentQuestion].choices[currentAnswer]);
-        $('#answerList').html("<img src='" + triviaQuestions[currentQuestion].imageLink + "'/>");
-		//Display Next question 
-
-    }
+        $('#answerList').html("<center><img class='img-thumbnail' src='" + triviaQuestions[currentQuestion].imageLink +"'/></center>");
+   	 }
 
  	function incorrectAnswers(answerID) {
+		console.log("InCorrect ans fun"+ timeRemaining);
 		//stop the clock 
 		stopCountDown(); 
 		//Count the in-correct answer 
 	    incorrectCount++;
 		//Display MEasges 
+		$("#timer").html("<h2> Time Remaining: 00:00 </h2> <br/>");
         $('#questionBlock').html("OH NO! THAT's not it <br />");
         $('#questionBlock').append("YOU CHOSE: " + triviaQuestions[currentQuestion].choices[answerID] + ".....HOWEVER THE ANSWER IS: " + triviaQuestions[currentQuestion].choices[currentAnswer]);
-        $('#answerList').html("<img src='" + triviaQuestions[currentQuestion].imageLink + "'/>");
+        $('#answerList').html("<center><img class='img-thumbnail' src='" + triviaQuestions[currentQuestion].imageLink + "'/></center>");
 	}
 	
 	
 	function timeOuts() {
+		
+		// clearInterval(showQA);
+		stopCountDown(); 
         unasweredCount++;
+		$("#timer").html("<h2> Time Remaining " + timeRemaining + "</h2> <br/>");
         $('#questionBlock').text("YOU FAILED TO CHOOSE AN ANSWER");
-        $('#answerList').html("<img src='"+triviaQuestions[currentQuestion].imageLink + "'/>");
-        stopCountDown(); 
+        $('#answerList').html("<center><img class='img-thumbnail' src='"+triviaQuestions[currentQuestion].imageLink + "'/></center>");
 	}
 	
+	function quizResults() {
+
+        // Timer is replaces with text
+        $("#timer").html("<h4>Times Up ! ALL Done!!</h4>");
+        $("btn-reset").attr("style", "display:block;");
+
+        $("#questionBlock").html("<h4>Correct Answers: " + correctCount + "</h4>");
+        $("#answerList").html("<h4>Wrong Answers: " + incorrectCount + "</h4>");
+        $("#answerList").append("<h4>UnAnswered: " + unasweredCount + "</h4>");
+	 }
 
 	function gameStart() {
 		//Calling the Timer function 
